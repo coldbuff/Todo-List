9af2f17acd16678f7400acd01e6b65dd00832aa1
“coldbuff“ <seongyungim428@gmail.com>
2025-02-27 11:22:38 +0900

    second

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { TodoTemplate } from './components/templates/TodoTemplate';
 import './styles/globals.css';
 
+// 연결
 function App() {
   return <TodoTemplate />;
 }
